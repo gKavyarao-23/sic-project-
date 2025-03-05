24b4893c78ed5add557c1deca61f7a71bc6e0fe7
G Kavya Rao <kavyarao.22cse@cambridge.edu.in>
2025-03-05 23:39:55 +0530

    message

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sic-project-
+# My name is Kavya
+# Tomorrow we have hackathon
+# We will win 1st prize
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import numpy as np
 print("hello world")
 a=np.mean([10,20,30])
 print(a)
+print("my name is Kavya")
